372ba62940a2d83431d9f3541118150b3c2b14e6
Alex Bruce <brucealmightyalex@yahoo.com>
2018-11-06 22:08:11 -0700

    Complete API tests

--- src/API/API.test.js
+++ src/API/API.test.js
@@ -1,7 +1,26 @@
 import * as API from './API';
 
 describe('API', () => {
-  it('should ', () => {
+  let url;
 
-  })
-})
+  beforeEach(() => {
+    url = 'https://tastedive.com/api/similar?k=322746-Apex-5CWVBUFX&info=1&type=music&q=Fall Out Boy';
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve({ data: 'response data' })
+    }));
+  });
+
+  it('should call fetchData with the correct url', () => {
+    API.fetchData('Fall Out Boy', 'music');
+
+    expect(window.fetch).toHaveBeenCalledWith(url);
+  });
+
+  it('should return a JSON object from the fetch call', async() => {
+    const expected = { data: 'response data' };
+    const response = await API.fetchData(url);
+
+    expect(response).toEqual(expected);
+  });
+});
